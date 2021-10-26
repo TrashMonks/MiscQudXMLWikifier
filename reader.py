c9ce819e596158393f2ffb8d5642d69d00f92130
Wreckstation <53310247+Wreckstation@users.noreply.github.com>
2021-10-26 16:57:20 -0400

    Update reader.py

--- reader.py
+++ reader.py
@@ -1,5 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 import xml.etree.ElementTree as etree
+import re
+import pyperclip
+
+
 baseloc = 'C:\Program Files (x86)\Steam\steamapps\common\Caves of Qud\CoQ_Data\StreamingAssets\Base'
+
+
+# Helper Functions
+
+def argsfortype(xmltype):
+    if (xmltype == "conversation"):
+        return ["name"]  # TODO: add optional "title" argument override
+    elif (xmltype == "colors" or xmltype == "bodyparts"):
+        return []
+    else:
+        return ["name"]
 
 def getbaseroot(baselocation, filename):
     et = etree.parse(baselocation + filename)
@@ -19,6 +37,8 @@ def wikilang(attrname):
         return "weight"
     elif attrname == "Name":
         return "table"
+    elif attrname == "chance":
+        return "weight"
     else:
         return "error"
     
@@ -50,7 +70,7 @@ def totemplate(node, template):
     #finalresult += '\n<noinclude>[[Category:Encounter Tables]]</noinclude>'
     return finalresult
 
-def todict(node):
+def tocolordict(node):
     d = {}
     for n in node.iter('shader'):
         d[n.get('Name')] = [n.get('Colors'), n.get('Type')]
@@ -72,67 +92,95 @@ def toconvo(node, title=None, ids=None):
     # {{Qud dialogue:choice row|tonode=end
     # |text=testing, testing!|end= true}}
     # {{!}}-}} UseID
-    qchoices = []
-    nquests = 1
-    for n in node.iter('choice'):
-        if nquests > 1:
-            qnum = 'quest2'
-            snum = 'step2'
-        else:
-            qnum = 'quest'
-            snum = 'step'
-        row = []
-        if n.get('UseID'):
-            row = ids[n.get('UseID')]
-        else:
-            row = [f'|tonode={n.get("GotoID")}',
-                   f'|text={n.text.strip()}']
-            if n.get('CompleteQuestStep'):
-                quest, step = n.get('CompleteQuestStep').split('~')
-                row.append(f'|{qnum}={quest}|{snum}={step}')
-                nquests = 2
-            elif n.get('FinishQuest'):
-                quest = n.get('FinishQuest')
-                row.append(f'|{qnum}={quest}|{snum}=rewards')
-                nquests = 2
-            elif n.get('StartQuest'):
-                quest = n.get('StartQuest')
-                row.append(f'|{qnum}={quest}|{snum}=accept')
-                nquests = 2
-            convoid = n.get('ID')
-            if convoid not in ids:
-                ids[convoid] = row
-        qchoices.append('{{Qud dialogue:choice row\n' + '\n'.join(row) + '}}')
+    finalqchoices = ""
+    if True:
+        qchoices = []
+        nquests = 1
+        for n in node.iter('choice'):
+            if nquests > 1:
+                qnum = 'quest2'
+                snum = 'step2'
+            else:
+                qnum = 'quest'
+                snum = 'step'
+            row = []
+            if n.get('UseID'):
+                row = ids[n.get('UseID')]
+            else:
+                row = [f'|tonode={n.get("GotoID")}',
+                       f'|text={n.text.strip()}']
+                if n.get('CompleteQuestStep'):
+                    quest, step = n.get('CompleteQuestStep').split('~')
+                    row.append(f'|{qnum}={quest}|{snum}={step}')
+                    nquests = 2
+                elif n.get('FinishQuest'):
+                    quest = n.get('FinishQuest')
+                    row.append(f'|{qnum}={quest}|{snum}=reward')
+                    nquests = 2
+                elif n.get('StartQuest'):
+                    quest = n.get('StartQuest')
+                    row.append(f'|{qnum}={quest}|{snum}=accept')
+                    nquests = 2
+                convoid = n.get('ID')
+                if convoid not in ids:
+                    ids[convoid] = row
+            qchoices.append('{{Qud dialogue:choice row\n' + '\n'.join(row) + '}}')
         
-    finalqchoices = '{{Qud dialogue:choice|\n' + '\n{{!}}-\n'.join(qchoices) + '}}'
+        finalqchoices = '{{Qud dialogue:choice|\n' + '\n{{!}}-\n'.join(qchoices) + '}}'
     return qdialogue + '\n' + finalqchoices
 
 def replaceshaders(text):
     return re.sub('({{)\s*(.+\s*\|.*}})', '\\1Qud shader no parse|\\2', text)
 
+def tobodypartvariantsdict(node):
+    d = {}
+    for n in node.iter('bodyparttypevariant'):
+        d[n.get('Type')] = n.get('VariantOf')
+    return d
+
+def toanatomy(node):
+    d = []
+    for n in node.iter('part'):
+        temp = {}
+        temp['type'] = n.get('Type')
+        if n.get('Laterality'):
+            temp['laterality'] = n.get('Laterality')
+        d.append(temp)
+    return d
+
+def dictconversion(temp, arglen):
+    final = []
+    for e in temp:
+        parens = ['', '']
+        if arglen > 1:
+            parens = ['{', '}']
+            value = ', '.join(temp[e])
+        else:
+            value = temp[e]
+        final.append(f"['{e}'] = {parens[0]}'{value}'{parens[1]}")
+    finalstr = ',\n'.join(final)
+    return finalstr
+
+def anatomytemplate(name, _type, laterality):
+    s = f"{{{{Anatomy|name={name}\n|type={_type}"
+    if laterality is not None:
+        s += f"|laterality={laterality}"
+    s += '}}'
+    return  s
+           
+
+
+# Main "get" functions
+
+
 def getencountertable(root, args):
-    """
-    returns a specific encountertable formatted for the wiki.
-    NOTE: builders and hints are ignored
-    
-    args parameters:
-      name   | the name of the encounter table
-    """
     tbl = []
     for node in root.iter('encountertable'):
         if (node.attrib.get('Name') == args['name']):
             tbl.append(totemplate(node, 'EncounterTable'))
     return '\n'.join(tbl)
 
-
 def getpopulationtable(root, args):
-    """
-    returns a specific population table formatted for the wiki.
-    NOTE: builders and hints are ignored
-    
-    args parameters:
-      name   | the name of the population table
-    """
     tbl = []
     for node in root.iter('population'):
         if (node.attrib.get('Name') == args['name']):
@@ -142,30 +190,15 @@ def getpopulationtable(root, args):
             else:
                 etstyle = '|roll=once'
             tbl.append(totemplate(n, 'EncounterTable' + etstyle))
-    return '\n'.join(tbl)
-
+    return f"=== {args['name']} ===\n" + '\n'.join(tbl)
+    
 def getcolortable(root, args):
-    """
-    returns the color templates formatted for the wiki.
-    this doesn't use any args.
-    """
-    temp = todict(root)
-    final = []
-    for e in temp:
-        final.append("['" + e + "']={'" + temp[e][0] + "', '" + temp[e][1] + "'},")
-    finalstr = '\n'.join(final)
-    return finalstr
+    temp = tocolordict(root)
+    return dictconversion(temp, 2)
 
 def getconversation(root, args):
-    """
-    returns an entire conversation formatted for the wiki.
-    
-    args parameters:
-      name   | the name of the convo id
-      title  | (optional) the title to set. the wiki by default uses the base page title. 
-               this is used if you want to override this.
-    """
     tbl = []
+    ids = {}
     if 'title' in args:
         title = args['title']
     else:
@@ -173,8 +206,29 @@ def getconversation(root, args):
     for node in root.iter('conversation'):
         if (node.attrib.get('ID') == args['name']):
             for n in node.iter('node'):
-                tbl.append(toconvo(n, title))
+                tbl.append(toconvo(n, title, ids))
     return '\n'.join(tbl)
+
+def getbodytypevariants(root, args):
+    temp = tobodypartvariantsdict(root)
+    return dictconversion(temp, 1)
+
+def getanatomies(root, args):
+    d = {}
+    final = []
+    for node in root.iter('anatomy'):
+        d[node.get('Name')] = toanatomy(node)
+    anatomy = []
+    for a in d:
+        for part in d[a]:
+            anatomy.append(anatomytemplate(a, part['type'],
+                part['laterality'] if ('laterality' in part) else None))
+    final.append('\n'.join(anatomy))
+    return '\n'.join(final)
+
+
+# In[12]:
+
 
 def main(tabletype, args):
     function = lambda x: 'function is not set'
@@ -191,16 +245,30 @@ def main(tabletype, args):
     elif tabletype == 'population':
         filename = '\PopulationTables.xml'
         function = getpopulationtable
+    elif tabletype == 'bodyparts':
+        filename= '\Bodies.xml'
+        function = getbodytypevariants
+    elif tabletype == 'anatomies':
+        filename= '\Bodies.xml'
+        function = getanatomies
     else:
         error('type not specified!')
     r = getbaseroot(baseloc, filename)
-    print(function(r, args))
-"""
-main takes 2 (sort of) arguments:
-  tabletype: the xml type you want to read: (encounter, colors, conversation)
-  args: a dictionary of arguments to pass which depend on the table type you select.
-"""
-if __name__ = '__main__':
-    args = {'name':'JoppaZealot'}
-    main('conversation', args)
-    
+    output = function(r, args)
+    pyperclip.copy(output)
+    print(output)
+
+# Main Function
+
+xmltype = input('Read from which table: [encounter|colors|conversation|population|bodyparts|anatomies]:\n')
+args = {}
+for arg in argsfortype(xmltype):
+    print(arg)
+    inputstr = input(f'Input {arg}: ')
+    args.update({arg: inputstr})
+
+main(xmltype, args)
+print("Output copied into clipboard.")
+
+
+
